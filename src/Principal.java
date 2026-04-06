ee0c813da3e77d17d7b48e8acdff83bdd22cd899
Haidukz <gustavorau250@gmail.com>
2026-04-06 18:14:58 -0300

    Implementando package, getters, setters e this

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoLancamento = 1980;
-        meuFilme.duracaoMinutos = 170;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoLancamento(1979);
+        meuFilme.setDuracaoMinutos(170);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações : " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+
     }
 }
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,61 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private boolean incluidoNoPlano;
+    private int anoLancamento;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private String genero;
+    private int duracaoMinutos;
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean IncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
