071e7f4648111a6ca34579a8ec7b7fb2c8f27f03
Haidukz <gustavorau250@gmail.com>
2026-04-06 19:50:28 -0300

    implementando funções como calcular tempo de duração de filmes, adicionando series,super classe e sub classes

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,15 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     static void main(String[] args) {
+
         Filme meuFilme = new Filme();
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoLancamento(1979);
         meuFilme.setDuracaoMinutos(170);
+        System.out.println("Duração do filme é de " + meuFilme.getDuracaoMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -15,6 +19,25 @@ public class Principal {
         System.out.println("Total de avaliações : " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
+        Serie serie = new Serie();
+        serie.setNome("Lost");
+        serie.setAnoLancamento(2000);
+        serie.exibeFichaTecnica();
+        serie.setTemporadas(10);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(50);
+        System.out.println("Duração da serie é " + serie.getDuracaoMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoLancamento(1999);
+        outroFilme.setDuracaoMinutos(160);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(serie);
+        System.out.println(calculadora.getTempoTotal());
 
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,61 +1,14 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private boolean incluidoNoPlano;
-    private int anoLancamento;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private String genero;
-    private int duracaoMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public boolean IncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public int getAnoLancamento() {
-        return anoLancamento;
-    }
-
-    public int getDuracaoMinutos() {
-        return duracaoMinutos;
-    }
-
-    public void exibeFichaTecnica() {
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoLancamento);
-    }
-
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setAnoLancamento(int anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
-
-    public void setDuracaoMinutos(int duracaoMinutos) {
-        this.duracaoMinutos = duracaoMinutos;
-    }
-
-    public double pegaMedia() {
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
+
